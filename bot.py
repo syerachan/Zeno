82db8aa83b80c19d34a3c296a5738c4b8e3dc3d3
GlowzSucks <53585351+GlowzSucks@users.noreply.github.com>
2019-08-04 16:35:59 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,26 +16,6 @@ async def on_ready():
 
 
 
-	
-#A SIMPLE TEST COMMAND
-@bot.command(pass_context=True)
-async def hi(ctx):
-	"""
-	Sends Hi with the user of the user that used the command
-	"""
-	await bot.say(f"Hello there{ctx.message.author.display_name}")
-  
-
-
-@bot.command(pass_context=True,name='copy')
-async def _copy(ctx,*,msg):
-	"""
-	Repearts what ever the user tpyes after s.copy
-	"""
-	await bot.say(msg) #Send the messages
-
-  
-  
   
 #IF YOU WISH TO HOST YOUR CODE PUBLICALLY HIDE YOUR TOKEN VIA METHOD BELOW
 #YOU CAN USE os.environ TO HIDE YOUR BOT TOKEN: SAVE YOUR BOT TOKEN AS THE NAME YOU GAVE IN os.environ['name'] ON HEROKU Config Vars
