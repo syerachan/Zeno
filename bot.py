ef84d378c5b0cdec93a5c3872a5cafebfb96913b
CharmingMother <nawhpongmaji1@gmail.com>
2019-08-04 12:46:13 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import os
 
 
 #GIVE YOUR BOT A PREFIX; mine is a.
-bot = commands.Bot(command_prefix="a.")
+bot = commands.Bot(command_prefix="s.")
 
 
 
@@ -27,7 +27,7 @@ async def hi(ctx):
   
 
 
-@bot.command(pass_context=True,name='copy') -> "Hello there!!"
+@bot.command(pass_context=True,name='copy')
 async def _copy(ctx,*,msg):
 	"""
 	Repearts what ever the user tpyes after s.copy
